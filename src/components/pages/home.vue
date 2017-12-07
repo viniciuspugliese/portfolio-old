a4a6def9258aa94ab149e3c225d416936b6045ff
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2017-12-07 14:40:46 -0200

    Pages path created

---
+++ src/components/pages/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <cHeader></cHeader>
+        <portfolio></portfolio>
+        <timeline></timeline>
+    </div>
+</template>
+
+<script>
+import cHeader from '../header'
+import portfolio from '../portfolio'
+import timeline from '../timeline'
+
+export default {
+    name: 'home',
+    components: {
+    	cHeader,
+    	portfolio,
+    	timeline
+    }
+}
+</script>
